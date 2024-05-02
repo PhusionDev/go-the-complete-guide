5719a611eeeb906d6089ce3073b4546d801a67eb
Jared Hodgkins <PhusionDev@gmail.com>
2024-05-02 06:31:52 -0400

    20240502 0631

--- 02-go-essentials/investment_calculator.go
+++ 02-go-essentials/investment_calculator.go
@@ -6,11 +6,47 @@ import (
 )
 
 func main() {
-	var investmentAmount = 1000
-	var expectedReturnRate = 5.5
-	var years = 10
+	const inflationRate = 2.5
 
-	var futureValue = float64(investmentAmount) * math.Pow((1 + expectedReturnRate / 100), float64(years))
+	var investmentAmount float64
+	var years float64
+	expectedReturnRate := 5.5
+
+	fmt.Print("Investment Amount: ")
+	fmt.Scan(&investmentAmount)
+
+	fmt.Print("Expected Return Rate: ")
+	fmt.Scan(&expectedReturnRate)
+
+	fmt.Print("Years: ")
+	fmt.Scan(&years)
+
+	futureValue := investmentAmount * math.Pow((1 + expectedReturnRate / 100), years)
+	futureRealValue := futureValue / math.Pow(1+inflationRate / 100, years)
 
 	fmt.Println(futureValue)
+	fmt.Println(futureRealValue)
+
+	CalculuateProfit()
+}
+
+func CalculuateProfit() {
+	var revenue, expenses, taxRate float64;
+
+	fmt.Print("Enter your revenue: ")
+	fmt.Scan(&revenue)
+	
+	fmt.Print("Enter your expenses: ")
+	fmt.Scan(&expenses)
+
+	fmt.Print("Enter your tax rate: ")
+	fmt.Scan(&taxRate)
+
+	var earningsBeforeTax float64 = revenue - expenses
+	var earningsAfterTax float64 = revenue * (1 - taxRate/100) - expenses
+	var ratio = earningsBeforeTax / earningsAfterTax
+
+	fmt.Println("Earnings before tax:", earningsBeforeTax)
+	fmt.Println("Profit:", earningsAfterTax)
+	fmt.Println("Ratio:", ratio)
 }
--- notes/packages.md
+++ notes/packages.md
@@ -2,5 +2,5 @@
 
 ## Built-in Packages
 
-- `fmt` - provides formatting to output
+- `fmt` - provides formatting to output, and general I/O
 - `math` - math functionality
